2eb45259cb97982665fb235b6a6ad3024b04f0a0
Jayson Harshbarger <jayson.harshbarger@analyst1.com>
2023-05-26 10:51:36 -0600

    using material theme

--- test/theme.test.js
+++ test/theme.test.js
@@ -18,3 +18,8 @@ it("renders css with prefixes", () => {
   const { css } = sass.compile("test/fixtures/with-prefix.scss");
   expect(css).toMatchSnapshot();
 });
+
+it("can use angular material theme", () => {
+  const { css } = sass.compile("test/fixtures/with-material.scss", { loadPaths: ["node_modules/"] });
+  expect(css).toMatchSnapshot();
+});
